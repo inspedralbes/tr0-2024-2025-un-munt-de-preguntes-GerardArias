29504113b36b241562255c266e8fcd7df0b90445
Gerard <a23gerarimar@insperalbes.cat>
2024-09-23 09:58:12 +0200

    Añadir fotos json + actualizacion js

--- web/funcio.js
+++ web/funcio.js
@@ -1,36 +1,56 @@
-function mostrarPreguntes(preguntes) {
-  const container = document.getElementById('partida'); 
-  let htmlString = '';  
+let preguntesGlobal = [];
+let preguntaActual = 0;
 
-  preguntes.forEach((preguntaObj, iPregunta) => {
-    const respostes = [...preguntaObj.respostes_incorrectes, preguntaObj.resposta_correcta].sort(() => Math.random() - 0.5);
-
-    htmlString += `<div style="margin-bottom: 20px;">`;
-    htmlString += `<p><strong>Pregunta ${iPregunta + 1}:</strong> ${preguntaObj.pregunta}</p>`;
-    
-    if (preguntaObj.imatge) {
-      htmlString += `<img src="${preguntaObj.imatge}" alt="Imatge de la pregunta" style="width: 200px; height: auto; margin-bottom: 10px;"><br>`;
-    }
-
-    respostes.forEach((resposta, iResposta) => {
-      htmlString += `<button onclick="resposta(${iPregunta}, ${iResposta})" style="margin-right: 10px; margin-bottom: 5px;">${resposta}</button>`;
-    });
-
-    htmlString += `</div>`;
-  });
-
-  container.innerHTML = htmlString;  
-}
-
-function resposta(iPregunta, iResposta) {
-  console.log(`Pregunta: ${iPregunta+1}, Resposta: ${iResposta+1}`);
-}
-
-fetch('http://localhost/tr0-2024-2025-un-munt-de-preguntes-GerardArias/back/getPreguntes.php')
-  .then(response => response.json()) 
-  .then(data => {
-    mostrarPreguntes(data.preguntes); 
+fetch('../back/getPreguntes.php')
+  .then(function(response) { return response.json(); })
+  .then(function(data) {
+    preguntesGlobal = data.preguntes;
+    mostrarPregunta(preguntaActual);
   })
-  .catch(error => {
+  .catch(function(error) {
     console.error('Error carregant les preguntes:', error);
   });
+
+function mostrarPregunta(iPregunta) {
+  let container = document.getElementById('partida');
+  let preguntaObj = preguntesGlobal[iPregunta];
+  let respostes = preguntaObj.respostes_incorrectes.concat([preguntaObj.resposta_correcta]).sort(function() { return Math.random() - 0.5; });
+
+  let htmlString = '<div style="margin-bottom: 20px;">';
+  htmlString += '<p><strong>Pregunta ' + (iPregunta + 1) + ':</strong> ' + preguntaObj.pregunta + '</p>';
+  
+  if (preguntaObj.imatge) {
+    htmlString += '<img src="' + preguntaObj.imatge + '" alt="Imatge de la pregunta" style="height: 200px; margin-bottom: 10px;"><br>';
+  }
+
+  respostes.forEach(function(resposta, iResposta) {
+    htmlString += '<button onclick="resposta(' + iPregunta + ', ' + iResposta + ')" style="margin-right: 10px; margin-bottom: 5px;">' + resposta + '</button>';
+  });
+
+  htmlString += '</div>';
+
+  //htmlString += '<button onclick="preguntaAnterior()" style="margin-right: 10px;">Anterior</button>';
+  //htmlString += '<button onclick="preguntaSiguiente()">Siguiente</button>';
+
+  container.innerHTML = htmlString;
+}
+
+
+function resposta(iPregunta, iResposta) {
+  console.log('Pregunta: ' + (iPregunta + 1) + ', Resposta: ' + (iResposta + 1));
+  preguntaSiguiente();
+}
+
+/*function preguntaAnterior() {
+  if (preguntaActual > 0) {
+    preguntaActual--;
+    mostrarPregunta(preguntaActual);
+  }
+}*/
+
+function preguntaSiguiente() {
+  if (preguntaActual < preguntesGlobal.length - 1) {
+    preguntaActual++;
+    mostrarPregunta(preguntaActual);
+  }
+}
