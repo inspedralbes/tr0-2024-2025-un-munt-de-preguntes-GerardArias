2ad68677fdc1e3fc8f6dccf86bb589f34491e3b5
Gerard <a23gerarimar@insperalbes.cat>
2024-09-30 10:50:59 +0200

    para crear base de datos

---
+++ back/insert.sql
@@ -0,0 +1,167 @@
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quin és l'equivalent en anglès de la senyal de trànsit 'STOP'?", 
+    "STOP", 
+    '["YIELD", "Caution", "Proceed"]', 
+    "Fotos/stop.jpg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quina és la velocitat màxima permesa en una zona urbana a Espanya?", 
+    "50 km/h", 
+    '["30 km/h", "60 km/h", "80 km/h"]', 
+    "Fotos/velocidad.jpg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quina senyal indica que hi ha una zona de construcció?", 
+    "Senyal de construcció", 
+    '["Senyal de perill", "Senyal de desviació", "Senyal de prohibició"]', 
+    "Fotos/construccio.png"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quina és la distància mínima que s'ha de mantenir amb el vehicle davant a l'autopista?", 
+    "2 segons", 
+    '["1 segon", "3 segons", "4 segons"]', 
+    "Fotos/distanciaseguridad.jpeg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quan es pot utilitzar el clàxon en una zona residencial?", 
+    "Només en casos d'emergència", 
+    '["En qualsevol moment", "Per avisar a altres conductors", "Per fer-se notar"]', 
+    "Fotos/claxon.jpeg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quina és la funció principal dels llums de boira?", 
+    "Millorar la visibilitat en condicions de boira", 
+    '["Il·luminar el camí en absència de llum", "Reduir l'enlluernament dels altres conductors", "Fer el vehicle més visible de nit"]', 
+    "Fotos/luzniebla.jpeg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Què significa una senyal de trànsit amb una fletxa que apunta cap avall?", 
+    "Que es requereix una parada completa", 
+    '["Que el trànsit està aturat", "Que es pot continuar sense parar", "Que es poden fer girs a la dreta"]', 
+    "https://exemple.com/down_arrow_sign.jpg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Com s'ha d'actuar si es veu una senyal de 'Ceda el paso'?", 
+    "Reduir la velocitat i cedir el pas als vehicles prioritats", 
+    '["Parar completament", "Augmentar la velocitat", "Seguir sense aturar-se"]', 
+    "Fotos/ceda.png"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quin tipus de senyal és una '-' blanc amb un fons vermell?", 
+    "Senyal de prohibició", 
+    '["Senyal d'advertència", "Senyal d'informació", "Senyal d'obligació"]', 
+    "Fotos/prohibido.png"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quina acció és recomanada si un vehicle davant teu s'atura inesperadament?", 
+    "Mantingues una distància segura i frena gradualment", 
+    '["Frena bruscament", "Canvia de carril immediatament", "Segueix sense canviar la velocitat"]', 
+    "Fotos/coche.jpeg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "En una intersecció sense senyals, qui té prioritat?", 
+    "El vehicle que entra des de la dreta", 
+    '["El vehicle que entra des de l'esquerra", "El vehicle que circula recte", "El vehicle que circula més ràpid"]', 
+    "Fotos/interseccion.avif"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Com s'ha de conduir en condicions de pluja intensa?", 
+    "Reduir la velocitat i augmentar la distància de seguretat", 
+    '["Augmentar la velocitat per evitar l'aigua acumulada", "Mantenir la velocitat normal i ajustar els llums", "Seguir sense fer cap canvi en la conducció"]', 
+    "Fotos/lluvia.jpeg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Què s'ha de fer quan es veu un semàfor en verd amb una fletxa verda?", 
+    "Continua amb precaució en la direcció indicada", 
+    '["Parar completament", "Girament a l'esquerra", "Accelerar"]', 
+    "Fotos/semaforo.jpeg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quan s'ha de posar el senyal de canvi de direcció?", 
+    "Abans de canviar de carril o fer un gir", 
+    '["Després de canviar de carril", "Quan el trànsit és dens", "Només si un altre vehicle ho indica"]', 
+    "Fotos/intermitente.jpeg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quina és la funció dels retrovisors en la conducció?", 
+    "Permetre veure els vehicles que venen per darrere i als costats", 
+    '["Illuminar la carretera", "Augmentar la velocitat", "Reduir el soroll del motor"]', 
+    "Fotos/retrovisor.jpeg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quin és el principal perill de conduir amb el telèfon mòbil?", 
+    "Disminució de l'atenció i concentració", 
+    '["Augment de la velocitat", "Millora en la comunicació", "Reducció de l'eficiència del vehicle"]', 
+    "Fotos/movil.jpeg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Com s'ha de reaccionar davant una senyal de 'Paret de trànsit'?", 
+    "Atura't completament i assegura't que el pas estigui lliure abans de continuar", 
+    '["Continua sense parar si el camí sembla lliure", "Redueix la velocitat i passa amb precaució", "Avisa amb el clàxon abans de passar"]', 
+    "https://exemple.com/stop_wall.jpg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quina acció es recomana si un vehicle et segueix de prop?", 
+    "Augmentar la distància de seguretat i mantenir una velocitat constant", 
+    '["Frena bruscament per fer-lo distanciar", "Augmentar la velocitat per escapar-ne", "Canviar de carril immediatament"]', 
+    "Fotos/perseguir.jpeg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quan està prohibit l'ús del clàxon en una autopista?", 
+    "En zones amb senyalització de prohibició específica", 
+    '["En tot moment durant la nit", "En condicions de boira", "Quan circules a baixa velocitat"]', 
+    "Fotos/claxon.jpeg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Quina senyal indica una zona escolar?", 
+    "Un senyal amb la imatge d'un nen caminant", 
+    '["Un senyal amb una fletxa ambar", "Un senyal amb un llibre", "Un senyal de velocitat reduïda"]', 
+    "https://exemple.com/school_zone.jpg"
+);
+
+INSERT INTO preguntes (pregunta, resposta_correcta, respostes_incorrectes, imatge) 
+VALUES (
+    "Què s'ha de fer si el vehicle derrapa en una carretera mullada?", 
+    "Reduir la velocitat i dirigir el vehicle en la direcció del derrapatge", 
+    '["Accelerar per recuperar el control", "Frenar bruscament", "Girar el volant en direcció contrària al derrapatge"]', 
+    "Fotos/derrape.jpg"
+);
