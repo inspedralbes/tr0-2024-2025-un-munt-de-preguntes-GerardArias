62bc4570870e985672ef05634d60d9f1a21e3f2f
Gerard <a23gerarimar@insperalbes.cat>
2024-10-02 08:45:57 +0200

    base de datos de remota

--- back/connect.php
+++ back/connect.php
@@ -4,5 +4,12 @@ $database = "UMDP";
 $username = "Gerard";
 $password = "Gerard1234";
 
+/*
+$servername = "localhost";
+$database = "a23gerarimar_Gerard";
+$username = "a23gerarimar_gerard";
+$password = "Gerard2005";
+*/
+
 $conn = mysqli_connect($servername, $username, $password, $database);
 ?>
